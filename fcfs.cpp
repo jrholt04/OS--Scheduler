7b289467180de9e94ecdf5a5776df106b8cf099f
jrholt04 <jrholt04@gmail.com>
2025-10-14 11:09:53 -0400

    commiting files for fcfs and main

---
+++ fcfs.cpp
@@ -0,0 +1,5 @@
+// File: fcfs.cpp
+// Author: Jackson Holt, Transy U
+// Course: CS 3074 Operating Systems
+//
+// Implementation of the First Come First Serve scheduling algorithm
---
+++ fcfs.h
@@ -0,0 +1,5 @@
+// File: fcfs.h
+// Author: Jackson Holt, Transy U
+// Course: CS 3074 Operating Systems
+//
+// header file for the First Come First Serve scheduling algorithm
