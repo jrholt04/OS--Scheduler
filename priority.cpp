5c087684c603c78a8b4905c74ed5f215c7f858d3
nneagle <nicholasaneagle@gmail.com>
2025-10-19 18:19:00 -0400

    initial upload of priority

---
+++ priority.cpp
@@ -0,0 +1,130 @@
+// File: priority.cpp
+// Author: J Holt, N Neagle, and A Seng, Transy U
+// Course: CS 3074 Operating Systems
+//
+// Implementation file for Priority Scheduler
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include "PCB.h"
+#include "priority.h"
+#include "schedulerTools.h"
+using namespace std;
+
+void prePriority(vector<PCB> tasks, bool verbose) {
+  vector<int> waitTimes(tasks.size(), 0);
+  vector<int> startTimes(tasks.size(), -1);
+
+  prePrioritySimulation(tasks, waitTimes, startTimes);
+
+  double avgWait = calculateAvgWait(waitTimes);
+  
+  if (verbose) {
+    printVerbose(tasks, startTimes, waitTimes, "Premptive Priority");
+  }
+  
+  cout << "Average wait time of Preemptive Priority: " << avgWait << endl; 
+  return;
+}
+
+void prePrioritySimulation(vector<PCB> PCBList, vector<int>& waitTimes, vector<int>& startTimes) {
+  int numberOfProcesses = PCBList.size();
+  vector<int> remainingTime(numberOfProcesses);
+  vector<bool> finished(numberOfProcesses, false);
+  
+  for (int i = 0; i < numberOfProcesses; i++) {
+    remainingTime[i] = PCBList[i].getBurst();
+  }
+  
+  int completed = 0;
+  int time = 0;
+  
+  while (completed < numberOfProcesses) {
+    int minIndex = -1;
+    int minPriority = 101;
+    
+    for (int i = 0; i < numberOfProcesses; i++) {
+      if (!finished[i] && (PCBList[i].getArrivalTime() <= time) && (PCBList[i].getPriority() < minPriority) && (remainingTime[i] > 0)) {
+	minPriority = PCBList[i].getPriority();
+	minIndex = i;
+      }
+    }
+    
+    if (minIndex == -1) {
+      time++;
+      continue;
+    }
+    
+    if (startTimes[minIndex] == -1) {
+      startTimes[minIndex] = time;
+    }
+    
+    for (int i = 0; i < numberOfProcesses; i++) {
+      if (i != minIndex && !finished[i] && PCBList[i].getArrivalTime() <= time) {
+	waitTimes[i]++;
+      }
+    }
+    
+    remainingTime[minIndex]--;
+    time++;
+    
+    if (remainingTime[minIndex] == 0) {
+      finished[minIndex] = true;
+      completed++;
+    }
+  }
+}
+
+void nonprePriority(vector<PCB> tasks, bool verbose) {
+  vector<int> startTimes(tasks.size(), -1);
+  vector<int> waitTimes(tasks.size(), 0);
+  
+  nonprePrioritySimulation(tasks, waitTimes, startTimes);
+  
+  double avgWait = calculateAvgWait(waitTimes);
+  
+  if (verbose) {
+    printVerbose(tasks, startTimes, waitTimes, "Non-Preemptive Priority");
+  }
+  
+  cout << "Average wait time of Non-Preemptive Priority: " << avgWait << endl;
+  
+  return;
+}
+
+void nonprePrioritySimulation(vector<PCB> PCBList, vector<int>& waitTimes, vector<int>& startTimes) {
+  int numberOfProcesses = PCBList.size();
+  int time = 0;
+  int completed = 0;
+  vector<bool> finished(numberOfProcesses, false);
+  
+  while (completed < numberOfProcesses) {
+    int minIndex = -1;
+    int minPriority = 101;
+    
+    for (int i = 0; i < numberOfProcesses; i++) {
+      if (!finished[i] && (PCBList[i].getArrivalTime() <= time) && (PCBList[i].getPriority() < minPriority)) {
+	minPriority = PCBList[i].getPriority();
+	minIndex = i;
+      }
+    }
+    
+    if (minIndex == -1) {
+      time++;
+      continue;
+    }
+    
+    if (startTimes[minIndex] == -1) {
+      startTimes[minIndex] = time;
+    }
+    
+    waitTimes[minIndex] = time - PCBList[minIndex].getArrivalTime();
+    
+    time += PCBList[minIndex].getBurst();
+    finished[minIndex] = true;
+    completed++;
+  }
+  return;
+}
---
+++ priority.h
@@ -0,0 +1,26 @@
+// File: priority.h
+// Author: J Holt, N Neagle, and A Seng, Transy U
+// Course: CS 3074 Operating Systems
+//
+// Header file for Priority Scheduler
+
+#ifndef PRIORITY_H
+#define PRIORITY_H
+
+#include <vector>
+#include "PCB.h"
+using namespace std;
+
+// Preemptive Priority Scheduler
+void prePriority(vector<PCB> tasks, bool verbose);
+
+// Runs the simulation for the Preemptive Priority Scheduler
+void prePrioritySimulation(vector<PCB> PCBList, vector<int>& waitTimes, vector<int>& startTimes);
+
+// Non Preemptive Priority Scheduler
+void nonprePriority(vector<PCB> tasks, bool verbose);
+
+// Runs the simulation for Non Preemptive Priority Scheduler
+void nonprePrioritySimulation(vector<PCB> PCBList, vector<int>& waitTimes, vector<int>& startTimes);
+
+#endif
