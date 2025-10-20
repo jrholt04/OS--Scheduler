2ea59e3f35897d812789858f8784b6b8399c0720
amseng26 <amseng26@transy.edu>
2025-10-20 10:21:51 -0400

    Updated rr

--- rr.cpp
+++ rr.cpp
@@ -2,13 +2,11 @@
 // Author: N Neagle, J Holt, and A Seng, Transy U
 // Course: CS 3074 Operating Systems
 //
-// Implementation of Round Robin
+// Round Robin
 
 #include <iostream>
 #include <vector>
-#include <string>
-#include <queue>
-#include <cstdio>
+#include <queue>           
 #include "PCB.h"
 #include "rr.h"
 #include "schedulerTools.h"
@@ -17,76 +15,76 @@ using namespace std;
 
 void rr(vector<PCB> tasks, bool verbose, int quanta) {
     vector<int> waitTimes(tasks.size(), 0);
-    vector<int> startTimes(tasks.size(), -1);
+    vector<int> startTimes(tasks.size(), -1); 
 
     rrSimulation(tasks, waitTimes, startTimes, quanta);
 
     double avgWait = calculateAvgWait(waitTimes);
-
+    
     if (verbose) {
         printVerbose(tasks, startTimes, waitTimes, "Round Robin");
     }
 
+    // Output the average wait time for the RR algorithm
     cout << "Average wait time of Round Robin: " << avgWait << endl;
 }
 
+//Round Robin simulation, calculating wait times and start times based on arrival order, time quanta, and remaining burst times.
 void rrSimulation(vector<PCB> PCBList, vector<int>& waitTimes, vector<int>& startTimes, int quanta) {
-    vector<PCB> readyList;
-    sortPCBArrivalTime(PCBList, readyList);
+    // Sort all processes by their arrival time
+    sort(PCBList.begin(), PCBList.end(), [](const PCB& a, const PCB& b) {
+        return a.getArrivalTime() < b.getArrivalTime();
+    });
 
-    int numberOfProcesses = readyList.size();
-    vector<int> remaining(numberOfProcesses);
-    vector<bool> finished(numberOfProcesses, false);
-    vector<bool> inQueue(numberOfProcesses, false);
-
-    for (int i = 0; i < numberOfProcesses; i++) remaining[i] = readyList[i].getBurst();
-
+    // Initialize remaining burst times for each process.
+    int n = PCBList.size();
+    vector<int> remainingBurst(n);
+    for (int i = 0; i < n; i++) {
+        remainingBurst[i] = PCBList[i].getBurst();
+    }
+    
     queue<int> readyQueue;
+
+    // Keep track of simulation time, number completed, and next process to add
     int time = 0;
     int completed = 0;
+    int nextProcess = 0;
 
-    while (completed < numberOfProcesses) {
-        // Add new processes to the queue
-        for (int i = 0; i < numberOfProcesses; i++) {
-            if (!inQueue[i] && !finished[i] && readyList[i].getArrivalTime() <= time) {
-                readyQueue.push(i);
-                inQueue[i] = true;
-            }
+    while (completed < n) {
+        while (nextProcess < n && PCBList[nextProcess].getArrivalTime() <= time) {
+            readyQueue.push(nextProcess);
+            nextProcess++;
         }
 
         if (readyQueue.empty()) {
             time++;
             continue;
         }
-
+        
         int i = readyQueue.front();
         readyQueue.pop();
 
-        if (startTimes[i] == -1) startTimes[i] = time;
-
-        int runTime = (remaining[i] < quanta) ? remaining[i] : quanta;
-        remaining[i] -= runTime;
+        if (startTimes[i] == -1) {
+            startTimes[i] = time;
+        }
+        
+        // Determine how long the process will run.
+        int runTime = min(quanta, remainingBurst[i]);
+        remainingBurst[i] -= runTime;
         time += runTime;
 
-        // Update waiting times for all other processes in queue
-        for (int j = 0; j < numberOfProcesses; j++) {
-            if (inQueue[j] && j != i && !finished[j])
-                waitTimes[j] += runTime;
+        // Add any new arrivals that occurred during that run.
+        while (nextProcess < n && PCBList[nextProcess].getArrivalTime() <= time) {
+            readyQueue.push(nextProcess);
+            nextProcess++;
         }
 
-        // Add any new arrivals
-        for (int j = 0; j < numberOfProcesses; j++) {
-            if (!inQueue[j] && !finished[j] && readyList[j].getArrivalTime() <= time) {
-                readyQueue.push(j);
-                inQueue[j] = true;
-            }
-        }
-
-        if (remaining[i] > 0) {
-            readyQueue.push(i); 
+        if (remainingBurst[i] > 0) {
+            readyQueue.push(i);
         } else {
-            finished[i] = true;
             completed++;
+            int finishTime = time;
+            waitTimes[i] = finishTime - PCBList[i].getArrivalTime() - PCBList[i].getBurst();
         }
     }
 }
